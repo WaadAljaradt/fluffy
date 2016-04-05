935a1c57ca0f3b427fb0ccbcbb7079d85aa67a66
Apoorv <patelapoorv9990@gmail.com>
2016-04-05 12:02:36 -0700

    Changed reset for when leader is dead

--- src/gash/router/server/EdgeCloseListener.java
+++ src/gash/router/server/EdgeCloseListener.java
@@ -26,13 +26,20 @@ public class EdgeCloseListener implements ChannelFutureListener {
         System.out.print("connection to "+edgeInfo.getRef()+" closed");
         EdgeMonitor.activeConnections.remove(edgeInfo.getRef());
 
-        if(ElectionHandler.getInstance().getLeaderNodeId() == edgeInfo.getRef() || EdgeMonitor.activeConnections.size() == 0)
+        if(ElectionHandler.getInstance().getLeaderNodeId() == edgeInfo.getRef() || (EdgeMonitor.activeConnections.size() == 0 && ElectionHandler.getInstance().getLeaderNodeId() == ElectionHandler.conf.getNodeId()))
         {
             ElectionHandler.getInstance().leaderIsDead();
-            System.out.println("The leader is dead!! The leader is dead!!");
+            if(EdgeMonitor.activeConnections.size() == 0 && ElectionHandler.getInstance().getLeaderNodeId() == ElectionHandler.conf.getNodeId())
+            {
+                System.out.println("Damn I am the leader and I am dead");
+            }
+            else
+            {
+                System.out.println("The leader is dead!! The leader is dead!!");
+            }
         }
 
-        Thread.sleep(new Random().nextInt(3) * 1000);
+//        Thread.sleep(new Random().nextInt(3) * 1000);
 
         ElectionHandler.getInstance().checkCurrentState();
     }
